5f8b82c1b6cb1c3fd965527f7b40a02e355e901b
Wumbei Divine <divinewumbei@gmail.com>
2023-09-08 06:44:37 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,8 @@
 #include <unistd.h>
-int main(void)
-{
-const char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, quote, 59); // 59 is the length of the quote excluding the null terminator
-return (1);
+int main(void) {
+char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+write(2, quote, 59); /* 59 is the length of the quote excluding the null terminator */
+
+return (1); // Return an error status code, e.g., 1, with parentheses
 }
